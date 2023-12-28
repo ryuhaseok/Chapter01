a8e57849dd7299b0f7db584c591cf41676f1610e
rhs0402 <rhs0402@naver.com>
2023-12-28 14:43:49 +0900

    *찍기

---
+++ src/com/javaex/ex04/Ex08.java
@@ -0,0 +1,20 @@
+package com.javaex.ex04;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		//세로
+		for (int i=1; i<7; i++) {
+			//가로
+			for (int s=1; s<6; s++) {
+				System.out.print("*");
+			}
+			//줄바꿈
+			System.out.println("*");
+		}
+		
+		
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex09.java
@@ -0,0 +1,22 @@
+package com.javaex.ex04;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+
+		//세로
+		for (int i = 1; i<7; i++) {
+			//가로
+			for (int z = 1; z!=i; z++) {
+				
+				System.out.print("*");
+			}
+			//줄바꿈
+			System.out.println("*");
+		}
+		
+		
+		
+	}
+
+}
